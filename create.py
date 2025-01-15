bfe52ae7a85dfa0e99aec6b9cd37370f09e1e48a
Chitranshi <chitranshi@zib.de>
2025-01-15 08:59:44 +0100

    Add support for Imagenet

--- create.py
+++ create.py
@@ -5,17 +5,18 @@ from typing import Any, Dict, Literal
 
 import torch
 from pytorch_lightning.lite import LightningLite
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, Compose, Resize, CenterCrop
+from torchvision.models import vgg19
 
 from utils.classifiers import ConvNet, WideResNet
 from utils.create import Create
-from utils.datasets import CIFAR10, FMNIST, MNIST
+from utils.datasets import CIFAR10, FMNIST, MNIST, IMAGENET
 from utils.utils import ModelWithNormalization, dataloader, set_seed
 
 class Main(LightningLite):
     def run(
         self,
-        dataset_name: Literal['MNIST', 'FMNIST', 'CIFAR10'],
+        dataset_name: Literal['MNIST', 'FMNIST', 'CIFAR10', 'IMAGENET'],
         mode: Literal['natural_rand', 'natural_det', 'uniform'], #, 'uniform_sub'],
         norm: Literal['L0', 'L2', 'Linf'],
         #large_epsilon: bool = False,
@@ -34,12 +35,13 @@ class Main(LightningLite):
 
         dataset_root = os.path.join(os.path.sep, '/home/htc/kchitranshi/SCRATCH', 'CFE_datasets')
 
-        ckpt_dir_path = os.path.join('/home/htc/kchitranshi/SCRATCH/CFE_models', dataset_name, 'version_0', 'checkpoints')
-        ckpt_name = [fname for fname in os.listdir(ckpt_dir_path) if '.ckpt' in fname][0]
-        ckpt_path = os.path.join(ckpt_dir_path, ckpt_name)
+        if dataset_name != 'IMAGENET':
+            ckpt_dir_path = os.path.join('/home/htc/kchitranshi/SCRATCH/CFE_models', dataset_name, 'version_0', 'checkpoints')
+            ckpt_name = [fname for fname in os.listdir(ckpt_dir_path) if '.ckpt' in fname][0]
+            ckpt_path = os.path.join(ckpt_dir_path, ckpt_name)
 
-        state_dict = torch.load(ckpt_path, map_location='cpu')['state_dict']
-        state_dict = OrderedDict((k.replace('classifier.', ''), v) for k, v in state_dict.items())
+            state_dict = torch.load(ckpt_path, map_location='cpu')['state_dict']
+            state_dict = OrderedDict((k.replace('classifier.', ''), v) for k, v in state_dict.items())
 
         if dataset_name in ('MNIST', 'FMNIST'):
             dataset_cls = MNIST if dataset_name == 'MNIST' else FMNIST
@@ -51,6 +53,10 @@ class Main(LightningLite):
             batch_size = 2500
             total_samples = 50000
             #target_classes = [3, 9] if norm == 'L2' else [0, 3]
+        elif dataset_name == 'IMAGENET':
+            dataset_cls = IMAGENET
+            batch_size = 500
+            total_samples = 10000
 
         else:
             raise ValueError(dataset_name)
@@ -61,7 +67,15 @@ class Main(LightningLite):
         atk_kwargs: Dict[str, Any] = {'norm': norm}
         
         if mode in ('natural_rand', 'natural_det'):
-            dataset = dataset_cls(dataset_root, True, ToTensor())
+            dataset = dataset_cls(dataset_root, True, ToTensor()) if dataset_name != 'IMAGENET' else dataset_cls('/software/ais2t/pytorch_datasets/imagenet/', True,transform=Compose([
+                Resize(256),
+                CenterCrop(224),
+                ToTensor(),
+            ]))
+            if dataset_name == 'IMAGENET':
+                indices = list(range(80000,100000)
+                               )
+                dataset = torch.utils.data.Subset(dataset, indices)
             loader = dataloader(dataset, batch_size, False)
             loader = self.setup_dataloaders(loader)
 
@@ -86,11 +100,19 @@ class Main(LightningLite):
             atk_kwargs['steps'] = 100 if norm in ('L2', 'Linf') else 100#150
             atk_kwargs['eps'] = 0.5 if norm == 'L2' else 0.1 if norm == 'Linf' else None
 
+        elif dataset_name == 'IMAGENET':
+            classifier = vgg19(weights='IMAGENET1K_V1')
+            atk_kwargs['steps'] = 100
+            atk_kwargs['eps'] = 0.1 if norm == 'L2' else 0.03 if norm == 'Linf' else None
+
         else:
             raise ValueError(dataset_name)
 
         classifier = ModelWithNormalization(classifier, dataset_cls.mean, dataset_cls.std)
-        classifier.load_state_dict(state_dict)
+        
+        if dataset_name != 'IMAGENET':
+            classifier.load_state_dict(state_dict)
+        
         classifier = self.setup(classifier)
         
         #set_seed(self.global_rank)
@@ -113,7 +135,7 @@ class Main(LightningLite):
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('dataset_name', choices=('MNIST', 'FMNIST', 'CIFAR10'))
+    parser.add_argument('dataset_name', choices=('MNIST', 'FMNIST', 'CIFAR10','IMAGENET'))
     parser.add_argument('mode', choices=(
         'natural_rand', 
         'natural_det', 
