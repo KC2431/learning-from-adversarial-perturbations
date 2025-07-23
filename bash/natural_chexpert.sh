46265461023f08280f310c37a550ef70ba5456ad
Chitranshi <chitranshi@zib.de>
2025-07-23 12:35:00 +0200

    Update hyperparams json file

---
+++ bash/natural_chexpert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eux
+
+mkdir -p logs
+
+now=$(TZ=UTC-9 date '+%Y-%m-%d-%H-%M-%S')
+
+devices=${@}
+norms=(L2 Linf GDPR_CFE SCFE)
+seeds=(10)
+seed=${seeds[0]}
+
+for norm in "${norms[@]}"; do
+        python3 natural_chexpert.py $norm $seed $devices hyperparams/hyperparams_chexpert.json >> logs/${now}.out 2>&1
+done
